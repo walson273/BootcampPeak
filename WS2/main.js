f57ac6914cd089a9114555ff1125940b272f48e3
SrTown <carloscofdi@gmail.com>
2024-04-18 22:04:09 -0500

    Folders

---
+++ WS2/main.js
@@ -0,0 +1,54 @@
+
+function calcularCalorias() {
+ //Formula hombres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) + 5
+ //Formula mujeres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) - 161
+}
+
+function mostrarMensajeDeError(msg) {
+    const calculo = document.querySelector('#calculo');
+    if (calculo) {
+        calculo.remove();
+    }
+
+    const divError = document.createElement('div');
+    divError.className = 'd-flex justify-content-center align-items-center h-100';
+    divError.innerHTML = `<span class="alert alert-danger text-center">${msg}</span>`;
+
+    resultado.appendChild(divError);
+
+    setTimeout(() => {
+        divError.remove();
+        desvanecerResultado();
+    }, 5000);
+}
+
+
+// Animaciones
+function aparecerResultado() {
+    resultado.style.top = '100vh';
+    resultado.style.display = 'block';
+    
+    let distancia = 100;
+    let resta = 0.3;
+    let id = setInterval(() => {
+        resta *= 1.1;
+        resultado.style.top = `${distancia - resta}vh`;
+        if (resta > 100) {
+            clearInterval(id);
+        }
+    }, 10)
+}
+
+function desvanecerResultado() {
+    let distancia = 1;
+
+    let id = setInterval(() => {
+        distancia *= 2;
+        resultado.style.top = `${distancia}vh`;
+        if (distancia > 100) {
+            clearInterval(id);
+            resultado.style.display = 'none';
+            resultado.style.top = 0;
+        }
+    }, 10)
+}
