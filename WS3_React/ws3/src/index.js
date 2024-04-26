e78e8d1982903535ddfacd1ab54479a6e2f16218
Walson <wch273@gmail.com>
2024-04-25 23:15:53 -0500

    react3

--- WS3_React/ws3/src/index.js
+++ WS3_React/ws3/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function HelloWorld() {
+  return <h1 className="greeting">Hello, world!</h1>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
