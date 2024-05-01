152f990e3b7ce77d10516f2e71e5c1275920b265
Walson <wch273@gmail.com>
2024-05-01 18:35:28 -0500

    El de verdad

--- WS3_React/ws3/src/componentes.js
+++ WS3_React/ws3/src/componentes.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import * as data from './Data.js'
 
-export function HelloWorld() {
-    return <h1 className="greeting">REACT PRUEBA!</h1>;
-  }
-export const ComponenteDesign = (argumento) =>{
+
+export const ComponenteLista = (argumento) =>{
     return(
       <React.Fragment>
         <div class="all-content">
           {
-            argumento.jsonsito.map((iteracion)=>
+            argumento.datos.map((iteracion)=>
               <React.Fragment>
+                 
+                 <section className='contenedor'>
+                    <Logo />
+                    <TituloPuesto />
+                    <Habilidades />
+                    <EmpresaNombre />
+                    <Ubicacion />
+                    <Salario />
+                    <Disponibles />
+                 </section>
+
                 <h2>{iteracion.nombre}</h2>
                 <p>{iteracion.edad}</p>
                 {
@@ -28,18 +37,5 @@ export const ComponenteDesign = (argumento) =>{
       </React.Fragment>
     );
   }
-export const Componente1 = () =>{
+
   
-    return(
-      <React.Fragment>
-        <ComponenteDesign jsonsito={data.funcionJson} />
-      </React.Fragment>
-    );
-  }
-export const CompoLogo = (loguisimont)=>{
-  return(
-    <React.Fragment>
-      <button type="button" className="btn btn-success">{loguisimont.loguisimo}</button>
-    </React.Fragment>
-  );
-}
