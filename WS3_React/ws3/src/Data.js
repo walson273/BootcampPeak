3decf2e38133fd8f69abd471af80e924212969c0
Walson <wch273@gmail.com>
2024-04-30 20:44:19 -0500

    data

---
+++ WS3_React/ws3/src/Data.js
@@ -0,0 +1,8 @@
+export const funcionJson=[
+    {'nombre':'Camilo', 'edad':'24', 'logo':['a','b','c','d'], 'img':'https://img.static-af.com/images/media/E56219E9-21C7-4BB0-B7A5F5353A5140D3/?w=320&aspect_ratio=2:1'},
+    {'nombre':'Carlos', 'edad':'24','logo':['a','b','c'], 'img':'https://upload.wikimedia.org/wikipedia/commons/e/e0/Air_india_b747-400_vt-esn_arp.jpg'},
+    {'nombre':'Liam', 'edad':'3.8','logo':['a','b'], 'img':'https://img.freepik.com/vector-gratis/ilustracion-contorno-avion-dibujado-mano_23-2150348345.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1714348800&semt=ais'},
+    {'nombre':'Angie', 'edad':'29','logo':['a'], 'img':'https://s1.elespanol.com/2023/11/11/actualidad/808929310_237509963_1706x960.jpg'}
+  ]
+
+  
