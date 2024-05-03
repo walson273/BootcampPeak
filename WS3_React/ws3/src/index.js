a22643b2c51b1d8f1f459b504f521982a48890bf
Oscar <oscarcruz744@gmail.com>
2024-05-02 21:01:27 -0500

    dashboard

--- WS3_React/ws3/src/index.js
+++ WS3_React/ws3/src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <compo.BSlide dat = {data.slideBar}/>
     <compo.ComponenteLista datos={data.funcionJson} />
   </React.StrictMode>
 );
