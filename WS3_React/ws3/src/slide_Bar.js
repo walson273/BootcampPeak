babefe9ef660f41418c8942a7718314ff2830c2b
Oscar <oscarcruz744@gmail.com>
2024-05-03 15:25:23 -0500

    yacasi

--- WS3_React/ws3/src/slide_Bar.js
+++ WS3_React/ws3/src/slide_Bar.js
@@ -2,9 +2,13 @@ import React from 'react';
 export const BSlide = (prop) => {
     return (
         <React.Fragment>
-            <div className='slidebar border bg-success'>
+            <div className='slidebar border'>
                 <nav>
                     <ul className='nav'>
+                    <div id="imagencita" className='border'>
+                            <img src="imagencitas/peaku.png" alt="Logo"/>
+                            <h2 id='letrita' >PeakU</h2>
+                    </div>
                         {prop.dat.map((iter) =>
                             <li className='box_items list-group-item nav-item '>
                                 <div className='cont_items'>
