a37c8b46c3f2e1a129b9ef52e144e96ca9ee30da
SrTown <carloscofdi@gmail.com>
2024-05-04 01:06:33 -0500

    Headers fin

--- WS3_React/ws3/src/componentes.js
+++ WS3_React/ws3/src/componentes.js
@@ -254,10 +254,10 @@ export const WhiteHeader = () =>{
                     <a href="#" class="dropdown__link"> <i class="ri-question-line"></i> Preguntas </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Profesores </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-pencil-ruler-line"></i> Profesores </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Bootcamp </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-bootstrap-line"></i> Bootcamp </a>
                   </li>
                 </ul>
               </li>
@@ -270,28 +270,28 @@ export const WhiteHeader = () =>{
                     <a href="#" class="dropdown__link"><i class="ri-pie-chart-line"></i> Inicio</a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Publicar empleo </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-account-box-line"></i> Publicar empleo </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Nuestro proceso </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-bar-chart-grouped-line"></i> Nuestro proceso </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Precios </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-money-dollar-circle-line"></i> Precios </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Pruebas Online </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-macbook-line"></i> Pruebas Online </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Nómina </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-wallet-line"></i> Nómina </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Blog </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-blogger-line"></i> Blog </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Comercial </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-megaphone-line"></i> Comercial </a>
                   </li>
                   <li>
-                    <a href="#" class="dropdown__link"> <i class="ri-arrow-up-down-line"></i> Caclculadora de salario </a>
+                    <a href="#" class="dropdown__link"> <i class="ri-calculator-line"></i> Caclculadora de salario </a>
                   </li>
                 </ul>
               </li>
@@ -337,15 +337,28 @@ export const BlueHeader = () =>{
   return(
     <React.Fragment>
       <div id="azulito">
-      <div id="search-div" class="input-group custom-input-group">
-        <div id="lupa" class="bg-light input-group-prepend">
-          <div class="input-group-text">
-            <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="search" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-search b-icon bi"><g data-v-08fb1bf6=""><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></g></svg>
+        <div id="search-div" class="input-group custom-input-group">
+          <div id="lupa" class="bg-light input-group-prepend">
+            <div class="input-group-text">
+              <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="search" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-search b-icon bi"><g data-v-08fb1bf6=""><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></g></svg>
+            </div>
           </div>
+          <input id="search-bar" type="search" placeholder="Busca por cargo, salario, ubicación o empresa" class="form-control form-control-md"/>
         </div>
-        <input id="search-bar" type="search" placeholder="Busca por cargo, salario, ubicación o empresa" class="form-control form-control-md"/>
-      </div>
-
+        <div id="filtros" class="text-md-right text-white text-truncate mt-2 mt-md-0 col-md-6 col-12">
+          <button type="button" class="btn btn-outline-light btn-sm rounded-pill">
+            Área<svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="caret down fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-caret-down-fill b-icon bi"><g data-v-7fda577a="" transform="translate(8 8) scale(0.7 0.7) translate(-8 -8)"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"></path></g></svg>
+          </button>
+          <button type="button" class="btn btn-outline-light btn-sm rounded-pill">
+            Cargo<svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="caret down fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-caret-down-fill b-icon bi"><g data-v-7fda577a="" transform="translate(8 8) scale(0.7 0.7) translate(-8 -8)"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"></path></g></svg>
+          </button>
+          <button type="button" class="btn btn-outline-light btn-sm rounded-pill">
+            Salario<svg  viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="caret down fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-caret-down-fill b-icon bi"><g data-v-7fda577a="" transform="translate(8 8) scale(0.7 0.7) translate(-8 -8)"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"></path></g></svg>
+          </button>
+          <button  type="button" class="btn btn-outline-light btn-sm rounded-pill">
+            Ubicación<svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="caret down fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-caret-down-fill b-icon bi"><g data-v-7fda577a="" transform="translate(8 8) scale(0.7 0.7) translate(-8 -8)"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"></path></g></svg>
+          </button>
+        </div>
       </div>
     </React.Fragment>
   );
