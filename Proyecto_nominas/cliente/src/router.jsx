cf1bce11dd0cdd4eca29f70fd83394bf370e56e9
Walson <wch273@gmail.com>
2024-05-17 15:45:15 -0500

    Merge branch 'main' of https://github.com/walson273/BootcampPeak

--- Proyecto_nominas/cliente/src/router.jsx
+++ Proyecto_nominas/cliente/src/router.jsx
@@ -25,6 +25,11 @@ export const router =  createBrowserRouter(
             path: '/login',
             element: <Login/>
 
+        },
+        {
+            path: '/buscar',
+            element: <Buscar />
+
         }
     
     ]
