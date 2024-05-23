3d92d36e1acb8662a5dd001cf9d56f51ad3cbc16
Oscar <oscarcruz744@gmail.com>
2024-05-23 02:59:40 -0500

    slidebar

--- Proyecto_nominas/cliente/src/views/Menu.jsx
+++ Proyecto_nominas/cliente/src/views/Menu.jsx
@@ -20,30 +20,32 @@ const BSlide = (prop) => {
   return (
     <React.Fragment>
       <div className='slidebar border' style={{ width: open ? '90px' : '210px', transition: open ? 'width 1s' : 'width 1s' }}>
-        <IoIosArrowDroprightCircle id='flechita' className={`${!open && "rotate-180"}`}  style={{margin: open ? '15rem 4.5rem' : '15rem 11.9rem', transition: open ? 'margin 1s' : 'margin 1s'}} onClick={() => setOpen(!open)} />
+        <IoIosArrowDroprightCircle id='flechita' className={`${!open && "rotate-180"}`}  style={{margin: open ? '0.9rem 4.5rem' : '0.9rem 11.9rem', transition: open ? 'margin 1s' : 'margin 1s'}} onClick={() => setOpen(!open)} />
         <div className='col'>
-          <div className='row-3' style={{height: "3.2rem", borderBottom: "0.5px solid gray"}}>
+        <img src="/Imagen_1.png" id='tele' alt="Logo"/>
+          <div className='row-3 ' style={{height: "4rem", borderBottom: "0.5px solid gray"}}>
             {/* <div className="row d-flex justify-content-center align-items-center"> */}
-            <div className='row'>
-              <div className='col-12 d-flex justify-content-center align-items-center bg-info' id='imagencita' style={{ marginLeft: open ? '0.2rem' : '-.4rem', transition: open ? 'marginLeft 1s' : 'marginLeft 1s' }}>
-                <img src="/Imagen_1.png" alt="Logo"/>
-                <h3  id="letrita" style={{fontSize: open ? '0rem' : '1rem'}}>TELEPERFUMES</h3>
-              {/* </div> */}
-              {/* <div className='col-9 border bg-black'>
-                <h3  id="letrita" >TELEPERFUMES</h3> */}
-              {/* </div> */}
+
+              <div className='d-flex justify-content-center align-items-center ' id='imagencita' >
+                
+                <h3  className={open ? 'letrita' : 'letrita active'} >TELEPERFUMES</h3>
             </div>
-            </div>
+
           </div>
           <div className='row-9 mt-3'>
             <nav>
               <ul className='nav'>
                 {prop.dat.map((iter) => (
-                  <li className='box_items list-group-item nav-item'>
+                  <li className='box_items list-group-item nav-item '>
                     <div className={open ? 'cont_items' : 'cont_items active'}>
-                      <a className={`btn btn-light ${open ? 'boton' : 'boton active'} `} href={iter.link} >
-                        <div className={open ? 'iconsize' : 'iconsize active'}>{iter.icon}</div>
-                        <h6 className={open ? 'nombreslide' : 'nombreslide active'} > {iter.name} </h6>
+                      <a className='btn btn-light iconsize' href={iter.link} >
+                        <div className='columnitas'>
+                          <div className='iconitos'>{iter.icon} </div>
+                        <div className='palabrita' style={{marginLeft: open ? '0.5rem' : '-2.5rem', transition: open ? 'marginLeft 1s' : 'marginLeft 1s'}}>
+                        <h6 style={{fontSize: open ? '0rem' : '1rem', transition: open ? 'fontSize 1s' : 'fontSize 1s'}} > {iter.name} </h6> 
+                        </div>
+                        {/* className={`border {open ? 'nombreslide' : 'nombreslide active'}`} */}
+                        </div>
                       </a>
                     </div>
                   </li>
