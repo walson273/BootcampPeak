8d9859fc83d041c10bf597d0f7f1076504e1cda7
SrTown <carloscofdi@gmail.com>
2024-05-27 01:13:58 -0500

    Retoques finales

--- Proyecto_nominas/cliente/src/views/Registrar_empleado.jsx
+++ Proyecto_nominas/cliente/src/views/Registrar_empleado.jsx
@@ -15,7 +15,6 @@ export default function Registrar_empleado() {
     const cargoIDUsuario = localStorage.getItem('CargoUsuario')
     if(cargoIDUsuario==5)
    {     
-    body.style.backgroundImage = "url('/public/dark-background-abstract-background-network-3d-background-7680x4320-8324.png')";
     return (
         <div id="fulldiv" style={{ margin: "0 10rem", padding: "0", paddingTop: "4%" }}>
             <Registro_empleado />
