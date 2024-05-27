720763b198293297e97e3071adf5db716e771d8a
Walson <wch273@gmail.com>
2024-05-26 23:59:55 -0500

    acceso denegado 5.0

--- Proyecto_nominas/cliente/src/views/Registrar_empleado.jsx
+++ Proyecto_nominas/cliente/src/views/Registrar_empleado.jsx
@@ -1,3 +1,4 @@
+import ACCESO_DENEGADO from "../components/ACCESO_DENEGADO";
 import Registro_empleado from "../components/rempleado.component";
 import { mostrar_cargos } from "../services/servicioCargos";
 import { mostrar_equipos } from "../services/ServicioEquipos";
@@ -10,10 +11,20 @@ export async function loader() {
 
 export default function Registrar_empleado() {
     const body = document.getElementById("bodyy");
+
+    const cargoIDUsuario = localStorage.getItem('CargoUsuario')
+    if(cargoIDUsuario==5)
+   {     
     body.style.backgroundImage = "url('/public/dark-background-abstract-background-network-3d-background-7680x4320-8324.png')";
     return (
         <div id="fulldiv" style={{ margin: "0 10rem", padding: "0", paddingTop: "4%" }}>
             <Registro_empleado />
         </div>
     )
+  }
+  else
+  {return(<>
+  
+    <ACCESO_DENEGADO></ACCESO_DENEGADO>
+  </>) }
 }
