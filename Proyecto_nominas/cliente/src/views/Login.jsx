5576b4a67527c5b5c384ec4215998e86b6a10320
Oscar <oscarcruz744@gmail.com>
2024-05-26 23:00:13 -0500

    asd

--- Proyecto_nominas/cliente/src/views/Login.jsx
+++ Proyecto_nominas/cliente/src/views/Login.jsx
@@ -73,7 +73,6 @@ export default function Login() {
               <div className="flex w-[32rem] h-[40rem] flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                   <CgProfile className="mx-auto h-14 w-auto text-white" />
-                  <CgProfile className="mx-auto h-14 w-auto text-white" />
                   <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Iniciar sesión en su cuenta</h2>
                 </div>
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
